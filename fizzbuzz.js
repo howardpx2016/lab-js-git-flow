b73f4b898b87304d7670bdd465d50cd796c2cfde
Howard Chen <cchen277@ucr.edu>
2019-08-04 22:13:35 +0800

    fizzbuzz function with bool added. tested with 60 as max_value to check fizbuzzbool

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,31 @@
 "use strict";
 
-for(let i = 0; i < 101; i++) {
-    if (i % 4 == 0 && i % 6 == 0) {
-        console.log("fizzbuzz");
-    }
-    else if (i % 4 == 0) {
-        console.log("fizz");
-    }
-    else if (i % 6 == 0) {
-        console.log("buzz");
-    }
-    else {
-        console.log(i);
+fizzbuzz(60);
+
+function fizzbuzz(max_val) {
+    let result;
+    for(let i = 0; i <= max_val; i++) {
+        if (i % 4 == 0 && i % 6 == 0) {
+            result = "fizzbuzz";
+        }
+        else if (i % 4 == 0) {
+            result = "fizz";
+        }
+        else if (i % 6 == 0) {
+            result = "buzz";
+        }
+        else {
+            result = i;
+        }
+
+        if (i % 5 == 0) {
+            if (result == i) {
+                result = "bool";
+            }
+            else {
+                result += "bool";
+            }
+        }
+        console.log(result);
     }
 }
