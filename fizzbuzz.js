5a013e60816946497fd4c8e80f6d274819d73464
Howard Chen <cchen277@ucr.edu>
2019-08-04 21:48:04 +0800

    functionalized fizzbuzz, tested with 25 as input

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,20 @@
 "use strict";
 
-for(let i = 0; i < 101; i++) {
-    if (i % 4 == 0 && i % 6 == 0) {
-        console.log("fizzbuzz");
-    }
-    else if (i % 4 == 0) {
-        console.log("fizz");
-    }
-    else if (i % 6 == 0) {
-        console.log("buzz");
-    }
-    else {
-        console.log(i);
+fizzbuzz(25);
+
+function fizzbuzz(max_val) {
+    for(let i = 0; i <= max_val; i++) {
+        if (i % 4 == 0 && i % 6 == 0) {
+            console.log("fizzbuzz");
+        }
+        else if (i % 4 == 0) {
+            console.log("fizz");
+        }
+        else if (i % 6 == 0) {
+            console.log("buzz");
+        }
+        else {
+            console.log(i);
+        }
     }
 }
