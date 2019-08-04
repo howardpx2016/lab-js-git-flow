b7f3129d14797637924710a375fc49a700a19ede
howardpx2016 <44950061+howardpx2016@users.noreply.github.com>
2019-08-04 21:38:32 +0800

    fizzbuzz merge (#1)
    * fizzbuzz from branch howardpx2016/fizzbuzz
    
    * fizzbuzz from branch howardpx2016/fizzbuzz, reduced spacing

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+"use strict";
+
+for(let i = 0; i < 101; i++) {
+    if (i % 4 == 0 && i % 6 == 0) {
+        console.log("fizzbuzz");
+    }
+    else if (i % 4 == 0) {
+        console.log("fizz");
+    }
+    else if (i % 6 == 0) {
+        console.log("buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
