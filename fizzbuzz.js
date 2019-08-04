276337a09702ea1ebe5835846616fbefad25eccd
howardpx2016 <44950061+howardpx2016@users.noreply.github.com>
2019-08-04 22:24:24 +0800

    functionalized fizzbuzz, tested with 25 as input (#2)
    * functionalized fizzbuzz, tested with 25 as input
    
    * modified function to accomodate for bool function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,22 @@
 "use strict";
 
-let result;
-for(let i = 0; i <= 100; i++) {
-    if (i % 4 == 0 && i % 6 == 0) {
-        result = "fizzbuzz";
+fizzbuzz(25);
+
+function fizzbuzz(max_val) {
+    let result;
+    for(let i = 0; i <= max_val; i++) {
+        if (i % 4 == 0 && i % 6 == 0) {
+            result = "fizzbuzz";
+        }
+        else if (i % 4 == 0) {
+            result = "fizz";
+        }
+        else if (i % 6 == 0) {
+            result = "buzz";
+        }
+        else {
+            result = i;
+        }
+        console.log(result);
     }
-    else if (i % 4 == 0) {
-        result = "fizz";
-    }
-    else if (i % 6 == 0) {
-        result = "buzz";
-    }
-    else {
-        result = i;
-    }
-    console.log(result);
 }
